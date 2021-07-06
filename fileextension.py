c4a1eff9a68d2b5895dc6f310ab28fc05c21df5f
Sanket1199 <86845483+Sanket1199@users.noreply.github.com>
2021-07-06 22:57:53 +0530

    Created a program to find extension from given input

---
+++ fileextension.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 22:32:01 2021
+
+@author: snkt8
+"""
+
+fn= input("Enter Filename: ")
+
+f = fn.split(".")
+
+print ("Extension of the file is : " + f[-1])
