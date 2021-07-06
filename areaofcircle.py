04580d83e097c348ddb2fe91e5ca1656a363cc88
Sanket1199 <86845483+Sanket1199@users.noreply.github.com>
2021-07-06 22:56:24 +0530

    Created a program to calculate area of circle

---
+++ areaofcircle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  6 22:39:11 2021
+
+@author: snkt8
+"""
+
+r = float(input("Enter radius of circle: "))
+a = 3.14159 * r * r
+print("Area of circle =", a)
